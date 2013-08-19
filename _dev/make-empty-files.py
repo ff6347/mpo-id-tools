f79d36cdf155294525a0554dbae3d34ac5b875b3
fabiantheblind <fabiantheblind@googlemail.com>
2013-08-19 10:04:14 +0200

    before moving to panel

---
+++ _dev/make-empty-files.py
@@ -0,0 +1,14 @@
+alist = ["003-013-jm2014-Info.txt",
+        "014-016-jm2014-Books.txt",
+        "018-103-jm2014-Guitars.txt",
+"104-151-jm2014-Drums.txt",
+"152-187-jm2014-Keys.txt",
+"188-197-jm2014-Zubehoer.txt",
+"198-239-jm2014-PA.txt",
+"240-255-jm2014-DJ.txt",
+"256-319-jm2014-Studio.txt",
+"320-322-jm2014-PiGal.txt",
+"323-337-jm2014-BrassNString.txt"]
+
+for x in alist:
+    file = open(x, 'w+')
